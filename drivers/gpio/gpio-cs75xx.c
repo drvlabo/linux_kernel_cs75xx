351dbacecb3ae7c81866ae21f4de5a35b0e89605
drvlabo <drvlabo@gmail.com>
2018-03-14 20:15:08 +0900

    update

--- drivers/gpio/gpio-cs75xx.c
+++ drivers/gpio/gpio-cs75xx.c
@@ -149,8 +149,8 @@ static int cs75xx_gpio_dir_out(struct gpio_chip *chip, unsigned int offset, int 
 
 	spin_lock_irqsave(&priv->lock, flags);
 
-	_gpio_dir(priv, offset, VAL_DIR_OUT);
 	_gpio_set(priv, offset, val);
+	_gpio_dir(priv, offset, VAL_DIR_OUT);
 
 	spin_unlock_irqrestore(&priv->lock, flags);
 
@@ -167,19 +167,18 @@ static void _toggle_irq_edge_trig(struct cs75xx_gpio *priv, unsigned int offs)
 	writel(regval ^ (0x01UL << offs), priv->reg_base + OFFS_LVL);
 }
 
+#if 0
 static irqreturn_t cs75xx_gpio_irq_handler(int irq, void *data)
 {
 	struct cs75xx_gpio *priv = data;
+	unsigned long flags;
 	unsigned int irq_stat;
 	unsigned int offset;
 	int cnt = 0;
-	unsigned long flags;
 
 	spin_lock_irqsave(&priv->lock, flags);
-
 	irq_stat = readl(priv->reg_base + OFFS_INT);
 	irq_stat &= ~(readl(priv->reg_base + OFFS_IE));
-
 	spin_unlock_irqrestore(&priv->lock, flags);
 
 	while (irq_stat) {
@@ -191,25 +190,67 @@ static irqreturn_t cs75xx_gpio_irq_handler(int irq, void *data)
 			spin_unlock_irqrestore(&priv->lock, flags);
 		}
 		generic_handle_irq(irq_find_mapping(priv->gpio_chip.irqdomain, offset));
-		cnt++;
 		irq_stat &= ~(0x01UL << offset);
+		cnt++;
 	}
 
 	return cnt ? IRQ_HANDLED : IRQ_NONE;
 }
+#else
+static void cs75xx_gpio_irq_handler(struct irq_desc *desc)
+{
+	struct cs75xx_gpio *priv;
+	unsigned int irq_stat;
+	unsigned int offset;
+	struct irq_chip *irqchip;
+	int k;
+	unsigned long flags;
+
+	priv = gpiochip_get_data(irq_desc_get_handler_data(desc));
+	irqchip = irq_desc_get_chip(desc);
+
+	chained_irq_enter(irqchip, desc);
+
+	spin_lock_irqsave(&priv->lock, flags);
+
+	irq_stat = readl(priv->reg_base + OFFS_INT);
+	irq_stat &= ~(readl(priv->reg_base + OFFS_IE));
+
+	spin_unlock_irqrestore(&priv->lock, flags);
+
+	for (k = 0; (k < GPIO_PIN_NUM) && irq_stat ; k++) {
+		unsigned int mask = 0x01UL << k;
+
+		if (!(irq_stat & mask))
+			continue;
+
+		if (priv->toggle_mask & mask) {
+			spin_lock_irqsave(&priv->lock, flags);
+			_toggle_irq_edge_trig(priv, k);
+			spin_unlock_irqrestore(&priv->lock, flags);
+		}
+		generic_handle_irq(irq_find_mapping(priv->gpio_chip.irqdomain, k));
+		irq_stat &= ~mask;
+	}
+
+	chained_irq_exit(irqchip, desc);
+}
+#endif
 
 static void cs75xx_gpio_irq_ack(struct irq_data *irq_data)
 {
 	struct cs75xx_gpio *priv;
 	unsigned long flags;
-	unsigned int regval;
 
 	priv = gpiochip_get_data(irq_data_get_irq_chip_data(irq_data));
 
 	spin_lock_irqsave(&priv->lock, flags);
 
 	writel(0x01UL << irq_data->hwirq, priv->reg_base + OFFS_INT);
-
+#if 0
+	writel(0xFFFFFFFF, priv->reg_base + OFFS_INT);
+	writel(0x00000000, priv->reg_base + OFFS_INT);
+#endif
 	spin_unlock_irqrestore(&priv->lock, flags);
 }
 
@@ -312,7 +353,7 @@ static int cs75xx_gpio_irq_set_type(struct irq_data *irq_data, unsigned int type
 }
 
 static struct irq_chip cs75xx_gpio_irqchip = {
-	.name	= "g2_gpio",			/* use short name here */
+	.name		= "g2_gpio",			/* use short name here */
 	.irq_ack	= cs75xx_gpio_irq_ack,
 	.irq_mask	= cs75xx_gpio_irq_mask,
 	.irq_unmask	= cs75xx_gpio_irq_unmask,
@@ -320,7 +361,11 @@ static struct irq_chip cs75xx_gpio_irqchip = {
 #ifdef NOT_YET
 	.irq_set_wake	= NULL,
 #endif
+#if 1
+	.flags		= IRQCHIP_MASK_ON_SUSPEND | IRQCHIP_SET_TYPE_MASKED,
+#else
 	.flags		= IRQCHIP_MASK_ON_SUSPEND,
+#endif
 };
 
 static int cs75xx_gpio_probe(struct platform_device *pdev)
@@ -401,20 +446,23 @@ static int cs75xx_gpio_probe(struct platform_device *pdev)
 	writel(0x00000000UL, priv->reg_base + OFFS_IE);
 
 	res = gpiochip_irqchip_add(gpio_chip, &cs75xx_gpio_irqchip, 0,
-					handle_bad_irq, IRQ_TYPE_NONE);
+					handle_level_irq, IRQ_TYPE_NONE);
 	if (res) {
 		dev_err(&pdev->dev, "failed to add irqchip.\n");
 		return res;
 	}
 
-	gpiochip_set_chained_irqchip(gpio_chip, &cs75xx_gpio_irqchip, priv->irq, NULL);
+printk("%s: this_irq = %d\n", __func__, priv->irq);
+#if 0
+	gpiochip_set_chained_irqchip(gpio_chip, &cs75xx_gpio_irqchip, priv->irq,
+					NULL);
 
 	res = devm_request_irq(&pdev->dev, priv->irq, cs75xx_gpio_irq_handler,
-			0, dev_name(&pdev->dev), priv);
-	if (res) {
-		dev_err(&pdev->dev, "failed to request_irq.\n");
-		return res;
-	}
+				0, "g2_gpio", priv);
+#else
+	gpiochip_set_chained_irqchip(gpio_chip, &cs75xx_gpio_irqchip, priv->irq,
+					cs75xx_gpio_irq_handler);
+#endif
 
 	return 0;
 }
